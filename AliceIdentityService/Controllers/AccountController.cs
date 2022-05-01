da122ca2341f611399d31e21b09ca4ab18990a7e
Chengyu Sun <csun@calstatela.edu>
2022-04-30 18:20:38 -0700

    Added Login and Register screens

---
+++ AliceIdentityService/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AliceIdentityService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AliceIdentityService.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new LoginInputModel());
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegistrationInputModel());
+        }
+    }
+}
+
+namespace AliceIdentityService.Models
+{
+    public class LoginInputModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegistrationInputModel
+    {
+        [Required, MaxLength(255), Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(255), Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, DataType(DataType.Password), Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
